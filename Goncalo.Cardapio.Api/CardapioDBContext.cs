4d2e9c73f96f02bbf87909a6af4701e1058d9c35
Gonçalo Henrique da Cruz <gonriq01@gmail.com>
2026-03-09 00:04:35 -0300

    Add files via upload
    trying to save project with errors

---
+++ Goncalo.Cardapio.Api/CardapioDBContext.cs
@@ -0,0 +1,13 @@
+namespace Goncalo.Cardapio.Api;
+
+using Microsoft.EntityFrameworkCore;
+
+public class CardapioDBContext : DbContext
+{
+    public CardapioDBContext(DbContextOptions<CardapioDBContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<ItemCardapio> Itens => Set<ItemCardapio>();
+}
---
+++ Goncalo.Cardapio.Api/ItemCardapio.cs
@@ -0,0 +1,9 @@
+namespace Goncalo.Cardapio.Api;
+
+public class ItemCardapio
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string description { get; set; }
+    public double price  { get; set; }
+}
