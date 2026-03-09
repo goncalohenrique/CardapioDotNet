68fbd4ecf5457699dd45ddbc7b978d598bfaa375
goncalohenrique <gonriq01@gmail.com>
2026-03-09 00:11:19 -0300

    feat: changing the gitignore

--- CardapioDBContext.cs
+++ CardapioDBContext.cs
@@ -5,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 public class CardapioDBContext : DbContext
 {
     public CardapioDBContext(DbContextOptions<CardapioDBContext> options) : base(options)
-    {
-        
-    }
+    { }
     
     public DbSet<ItemCardapio> Itens => Set<ItemCardapio>();
 }
